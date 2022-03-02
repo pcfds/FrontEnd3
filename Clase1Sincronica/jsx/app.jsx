5ba7ae958611f2a9a4560ce8e596df6c8d3c88c5
pcfds <castrofeijoopedro@gmail.com>
2022-03-02 18:57:13 -0300

    agregando cosas de React

---
+++ Clase1Sincronica/jsx/app.jsx
@@ -0,0 +1,20 @@
+const create = React.createElement;
+
+const span = create(
+    'span',
+    {key:2},
+    'Mundo!'
+)
+
+const p = create(
+    'p',
+    {key:1},
+    ['Hola ' ,span]
+
+
+)
+
+ReactDOM.render(
+    p,
+    document.getElementById("root")
+)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Trabajando con REACT
+Front End 3 - Certified Tech Developer
+
+Clase 1: REACT 
