dbcede8d1ca31bf09116b5f95dd21b03d3a6ec8e
pcfds <castrofeijoopedro@gmail.com>
2022-03-09 20:45:51 -0300

    agregando react nuevo

---
+++ C4Asincronica/my-app/src/componentes/ListaAnimales.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const animales = [
+  {
+    nombre: "Firulais",
+    edad: 11,
+    sexo: "Macho",
+    raza: "Pastor Aleman",
+    tamanio: "Grande",
+    id: 1,
+  },
+  {
+    nombre: "Maya",
+    edad: 13,
+    sexo: "Hembra",
+    raza: "Pug",
+    tamanio: "Chico",
+    id: 4,
+  },
+  {
+    nombre: "Pipo",
+    edad: 5,
+    sexo: "Macho",
+    raza: "Schnauzer",
+    tamanio: "Mediano",
+    id: 2,
+  },
+  {
+    nombre: "Pacha",
+    edad: 2,
+    sexo: "Hembra",
+    raza: "Mestizo",
+    tamanio: "Chico",
+    id: 3,
+  },
+];
+
+class ListaAnimales extends React.Component {
+  render() {
+    return (document.getElementById("root").innerHTML = (
+      <div>
+        {animales.map((animal) => (
+          <li key={animal.id}>
+            <h1>
+              <span>Nombre: </span>
+              {animal.nombre}
+            </h1>
+            <p>
+              <span>Edad:</span> {animal.edad}
+            </p>
+            <p>
+              <span>Sexo:</span> {animal.sexo}
+            </p>
+            <p>
+              <span>Raza:</span> {animal.raza}
+            </p>
+            <p>
+              <span>Tamaño:</span> {animal.tamanio}
+            </p>
+          </li>
+        ))}
+      </div>
+    ));
+  }
+}
+
+export default ListaAnimales;
